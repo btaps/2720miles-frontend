ae26a1bf8e44df5ee2f97c775853bdc2132acfb3
Los Tapias <lostapias@Loss-MacBook-Pro.local>
2020-01-31 23:22:11 -0800

    testing CORS with NGINX

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import  { withRouter } from 'react-router-dom'
+import User from './models/UsersModel.js'
 import './App.css'
 
 import Header from './containers/Header'
@@ -12,9 +13,17 @@ class App extends Component{
 
   state = {
     currentUser: localStorage.getItem('uid'),
-    id: ''
+    id: '',
+    allUsers: []
   }
-
+  
+  componentDidMount(){
+    User.all()
+      .then(data=> {
+	this.setState({allUsers: data})
+      })
+      .catch(err=> console.log('err fetchin all users', err))
+  }
 
   setCurrentUser = (token, userId) => {
 	  this.setState({
